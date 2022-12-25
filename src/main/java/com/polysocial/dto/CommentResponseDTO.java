b74f42b96bdc2a2a4fb291853c8f43f7b171a542
Phan Nguyen Dang Truong <96562712+phannguyendangtruong@users.noreply.github.com>
2022-12-25 22:03:14 +0700

    Update CommentResponseDTO.java

--- src/main/java/com/polysocial/dto/CommentResponseDTO.java
+++ src/main/java/com/polysocial/dto/CommentResponseDTO.java
@@ -16,6 +16,7 @@ import lombok.NoArgsConstructor;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CommentResponseDTO {
+    private Long cmtId;
     
     private String content;
 
